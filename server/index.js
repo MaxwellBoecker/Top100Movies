4f607c9123e18682773792d2edf0442fb714b459
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-19 15:01:13 -0600

    (feat) added user route and body parser

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 require('dotenv').config();
 const { movieRouter } = require('./movies');
+const { userRouter } = require('./users');
+const bodyParser = require('body-parser');
 const { PORT } = process.env;
 const app = express();
-
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.status(200).send('You have reached the server');
 });
 
 app.use('/movies', movieRouter);
+app.use('/users', userRouter);
 app.listen(PORT, () => {
   console.log(`app listening on ${PORT}`);
 });
