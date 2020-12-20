269fddbd80f4f794566641d1cbafe1b0fa06a112
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-19 22:46:02 -0600

    (feat) login redirects to /auth/google

---
+++ src/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Login = () => {
+  const onClick = () => {
+
+  };
+  return (
+    <div style={{ color: 'white' }}>
+      <a href="/auth/google">
+        <button type="button">Login</button>
+      </a>
+
+    </div>
+  );
+};
+
+export default Login;
