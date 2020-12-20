e27ab37711688793e1c9931dc1e2ac8c3c9b0e7b
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-19 19:44:39 -0600

    (feat) auth router imported

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const passport = require('passport');
 const { movieRouter } = require('./movies');
 const { userRouter } = require('./users');
+const { authRouter } = require('./auth');
 
 const { PORT } = process.env;
 const app = express();
@@ -10,13 +13,20 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.status(200).send('You have reached the server');
 });
-
+// app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+// app.get('/auth/google/callback',
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   (req, res) => {
+//     res.redirect('/');
+//   });
+app.use('/auth', authRouter);
 app.use('/movies', movieRouter);
 app.use('/users', userRouter);
+
 app.listen(PORT, () => {
   console.log(`app listening on ${PORT}`);
 });
 
 module.exports = {
   app,
-}
+};
