9b56fd89cffadbaf025d1ff12cae098a4923ad26
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-19 22:45:22 -0600

    (feat) router working. home page is login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Login from './Login.jsx';
 
-const App = () => {
-  return (
-    <div style={ {color: 'white' } }>Welcome to Top 100 movies </div>
-  )
-}
+const App = () => (
+  // <div style={ {color: 'white' } }>Welcome to Top 100 movies </div>
+  <Router>
+    <div style={{ color: 'white' }}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/movielist">Movies</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
