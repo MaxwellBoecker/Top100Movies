4b6c18482472195f845ffe84bd00fcfc6e3a7318
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-23 17:51:11 -0600

    (feat) added logout button

---
+++ src/Logout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logout = () => (
+  <div style={{ color: 'white' }}>
+    <a href="/auth/logout">
+      <button type="button">Logout</button>
+    </a>
+
+  </div>
+);
+
+export default Logout;
