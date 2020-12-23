788e3ba6c584f092cba78cfb18587b2609e8a58d
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-23 17:50:12 -0600

    (feat) added cookieSession

--- server/index.js
+++ server/index.js
@@ -3,35 +3,46 @@ const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const passport = require('passport');
+const cookieSession = require('cookie-session');
 const { movieRouter } = require('./movies');
 const { userRouter } = require('./users');
 const { authRouter } = require('./auth');
+const { pool } = require('../database');
 
 const { PORT } = process.env;
 const app = express();
 app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, '../build')));
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: ['id'],
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
 
 passport.serializeUser((id, done) => {
-  console.log(id, 'yolo');
   done(null, { id });
 });
 passport.deserializeUser((id, done) => {
-  //sql query in here
-  console.log('in here deser')
-  done(null, id);
+  pool.query('select * from users where google_id = ($1)', [id], (err, resp) => {
+    if (err) {
+      console.log(err);
+      done(err);
+    } else {
+      console.log('successfully deserialized');
+      done(null, id);
+    }
+  });
 });
-app.use(passport.initialize());
-app.use(passport.session());
-// app.get('/', (req, res) => {
-//   res.status(200).send('You have reached the server');
-// });
-// app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-// app.get('/auth/google/callback',
-//   passport.authenticate('google', { failureRedirect: '/login' }),
-//   (req, res) => {
-//     res.redirect('/');
-//   });
+
 app.use('/auth', authRouter);
 app.use('/movies', movieRouter);
 app.use('/users', userRouter);
@@ -41,5 +52,5 @@ app.listen(PORT, () => {
 });
 
 module.exports = {
-  app,
+  isLoggedIn,
 };
