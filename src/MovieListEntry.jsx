fe2f94931529816ff8da6b94f1e35aed8fb8a57c
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-31 12:13:43 -0600

    (feat) jpg image successfully rendering

--- src/MovieListEntry.jsx
+++ src/MovieListEntry.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
 const MovieListEntry = (props) => {
+  const path = 'https://image.tmdb.org/t/p/original';
   const { movieData } = props;
   const {
     title, poster_path, overview, backdrop_path, original_language,
   } = movieData;
+  console.log(poster_path)
   return (
     <div style={{ color: 'white' }}>
       <div>{title}</div>
-      <img alt="movie poster" src={poster_path} />
+      <img alt="movie poster" src={poster_path !== null ? path + poster_path : ''} style={{ width: '15%', height: '15%'}}/>
+      <div>{overview}</div>
+      <div>original language: {original_language}</div>
     </div>
   );
 };
