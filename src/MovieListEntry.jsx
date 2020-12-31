25463eac2785eeadb5b3436e4e27db4528482436
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-31 12:33:24 -0600

    (feat) delete works properly in child

--- src/MovieListEntry.jsx
+++ src/MovieListEntry.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 
 const MovieListEntry = (props) => {
   const path = 'https://image.tmdb.org/t/p/original';
-  const { movieData } = props;
+  const { movieData, deleteUserMovie } = props;
   const {
-    title, poster_path, overview, backdrop_path, original_language,
+    title, poster_path, overview, backdrop_path, original_language, id,
   } = movieData;
-  console.log(poster_path)
   return (
     <div style={{ color: 'white' }}>
       <div>{title}</div>
-      <img alt="movie poster" src={poster_path !== null ? path + poster_path : ''} style={{ width: '15%', height: '15%'}}/>
+      <img alt="movie poster" src={poster_path !== null ? path + poster_path : ''} style={{ width: '15%', height: '15%' }} />
       <div>{overview}</div>
-      <div>original language: {original_language}</div>
+      <div>
+        original language:
+        {original_language}
+      </div>
+      <button onClick={()=> deleteUserMovie(id)}>
+      Remove from favorites
+      </button>
     </div>
   );
 };
