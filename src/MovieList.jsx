8460e932405221bf485ff1529722dace4d8e628d
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-31 12:32:50 -0600

    (feat) delete axios request

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -6,14 +6,18 @@ const MovieList = () => {
   const [movies, setMovies] = useState([]);
   useEffect(async () => {
     const resp = await axios.get('/usermovie');
-    console.log(resp.data);
     setMovies(resp.data);
   }, []);
+
+  const deleteUserMovie = async (id) => {
+    const confirmation = await axios.delete('usermovie', { data: { id } });
+    console.log(confirmation);
+  };
 
   return (
     <div>
       <div style={{ color: 'white' }}>
-        {movies.map((m) => <MovieListEntry movieData={m} key={m.id} />)}
+        {movies.map((m) => <MovieListEntry movieData={m} key={m.id} deleteUserMovie={deleteUserMovie} />)}
       </div>
     </div>
   );
