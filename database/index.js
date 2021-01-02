361ac965cd4a2b36cda4c36ac7a8b33731fccbca
MaxwellBoecker <boecker.maxwell@gmail.com>
2021-01-02 13:34:27 -0600

    (refactor) specify prod db

--- database/index.js
+++ database/index.js
@@ -3,12 +3,12 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const {
-  DATABASE, PASSWORD, HOST, USER, DATABASE_URL,
+  DATABASE, PASSWORD, HOST, USER, DATABASE_URL, HEROKU_DATABASE
 } = process.env;
 const pool = new Pool({
   user: USER,
   host: DATABASE_URL || 'localhost',
-  database: DATABASE,
+  database: HEROKU_DATABASE || DATABASE,
   password: PASSWORD,
 });
 
