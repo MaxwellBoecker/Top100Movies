be157c506bb1a50b6adec0f4848e0fefdbd4eace
MaxwellBoecker <boecker.maxwell@gmail.com>
2021-01-02 13:19:46 -0600

    (refactor) changing DB connection URL

--- database/index.js
+++ database/index.js
@@ -3,14 +3,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const {
-  DATABASE, PASSWORD, HOST, USER,
+  DATABASE, PASSWORD, HOST, USER, DATABASE_URL,
 } = process.env;
 const pool = new Pool({
   user: USER,
-  host: HOST || 'localhost',
+  host: DATABASE_URL || 'localhost',
   database: DATABASE,
   password: PASSWORD,
-  port: 5432,
 });
 
 module.exports = {
