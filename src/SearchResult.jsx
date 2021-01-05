484f0fc2b6a7e35b5b39160bc01330db15ce69e0
MaxwellBoecker <boecker.maxwell@gmail.com>
2021-01-04 21:52:51 -0600

    (refactor) added image to search result

--- src/SearchResult.jsx
+++ src/SearchResult.jsx
@@ -6,12 +6,14 @@ const SearchResult = (props) => {
   const {
     title, original_language, overview, release_date, poster_path, backdrop_path, id,
   } = movieInfo;
+  const path = 'https://image.tmdb.org/t/p/original';
 
   return (
     <div className="search-result" style={{ color: 'white' }}>
       <div style={{ color: 'purple' }}>{title}</div>
       <div style={{ color: 'red' }}>{original_language}</div>
       <div style={{ color: 'goldenrod' }}>{release_date}</div>
+      <img alt="movie poster" src={poster_path !== null ? path + poster_path : ''} style={{ width: '15%', height: '15%' }} />
       <p>{overview}</p>
       <button onClick={() => addFavoriteMovie({
         title,
