f7e0c7f00d0a7d20bbab6e8a0f7637f21cf9f651
MaxwellBoecker <boecker.maxwell@gmail.com>
2021-01-18 21:01:54 -0600

    (feat) created theme, added to src index

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,5 +1,14 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App.jsx';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.getElementById('app'),
+);
